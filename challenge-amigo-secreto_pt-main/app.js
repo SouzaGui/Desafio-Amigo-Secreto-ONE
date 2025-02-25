fe7ae687c7967036ff903692e034bdc59256ff6b
Guilherme <guiilherme.souza42@gmail.com>
2025-02-25 14:38:41 -0300

    Adicionando função para exibir nomes adicionados e função de sortear

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -8,8 +8,36 @@ function adicionarAmigo(){
         amigos.push(nome);
     }
     document.getElementById("amigo").value='';
+
+    
+
+    listaAmigos = document.getElementById("listaAmigos");
+
+    listaAmigos.innerHTML = "";
+
+
+    
+    for(let i = 0; i < amigos.length; i++){
+        let lista = document.createElement("li");
+        lista.innerHTML = amigos[i];
+        listaAmigos.appendChild(lista);
+    }
 }
 
+function sortearAmigo(){
+    if(amigos.length == 0){
+        alert("Você precisa adicionar nomes a serem sorteados !");
+    }else if(amigos.length == 1){
+        alert("Você precisa adicionar mais de um nome para sortear !");
+    }
+    
 
-//Verificar se o nome foi adicionado corretamente ao array amigos
-//console.log(amigos) 
+    sorteado = amigos[Math.floor(Math.random() * amigos.length)];
+
+    console.log(sorteado);
+
+    resultado = document.getElementById("resultado");
+    resultado.innerHTML = "O nome sorteado é: " + sorteado;
+
+}
+
